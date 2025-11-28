1d0511ba57c5b041e7ba2e6489a166303356a07a
何 毅 <k.ka_oses@PCIT000094.local>
2025-11-28 15:40:21 +0900

    fix: add colortheme and markdown preview

--- init.lua
+++ init.lua
@@ -1160,25 +1160,43 @@ require('lazy').setup({
     },
   },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+  -- You can easily change to a different colorscheme.
+  -- Change the name of the colorscheme plugin below, and then
+  -- change the command in the config to whatever the name of that colorscheme is.
+  --
+  -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+  {
     'folke/tokyonight.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
       ---@diagnostic disable-next-line: missing-fields
       require('tokyonight').setup {
         styles = {
-          comments = { italic = false }, -- Disable italics in comments
+          comments = { italic = true }, -- Disable italics in comments
         },
       }
 
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      -- vim.cmd.colorscheme 'tokyonight-night'
+    end,
+  },
+
+  {
+    'loctvl842/monokai-pro.nvim',
+    priority = 1000,
+    config = function()
+      require('monokai-pro').setup {
+        filter = 'pro', -- 可选: classic | octagon | pro | machine | ristretto | spectrum
+        background_clear = {
+          -- 'neo-tree',
+          -- 'bufferline',
+          'telescope',
+          -- 'toggleterm',
+        },
+      }
+      vim.cmd.colorscheme 'monokai-pro'
     end,
   },
 
@@ -1307,5 +1325,6 @@ require('lazy').setup({
 })
 
 require('custom/chatgpt_sender').setup()
+require('custom/cursor_sender').setup()
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
